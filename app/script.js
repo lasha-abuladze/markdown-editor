6a2dce966229cc1bd6514a98dd93c0985d772b3d
Lasha Abuladze <lashaa.abuladzee@gmail.com>
2026-06-03 13:07:26 +0400

    feat: implement core UI functionality

--- app/script.js
+++ app/script.js
@@ -4,9 +4,33 @@
 const sectionPreview = document.querySelector(`.section--preview`);
 const btnHideShow = document.querySelector(`.btn--hide-show`);
 const iconHideShow = document.querySelectorAll(`.icon--hide-show`);
+const btnsMenu = document.querySelectorAll(`.btn--menu`);
+const mainSection = document.querySelector(`.main`);
+const btnThemeSwitcher = document.querySelector(`.theme-switcher`);
+const circleThemeSwitcher = document.querySelector(`.theme-switcher--circle`);
+const IconsThemeSwitcher = document.querySelectorAll(`.icon--theme-switcher`);
+// const iconsHideShow = document.querySelectorAll(`.icon--hide-show`);
+
 
 sectionPreview.classList.add(`display-none`);
+
+
 btnHideShow.addEventListener(`click`, () => {
     sectionPreview.classList.toggle(`display-none`);
     iconHideShow.forEach(icon => icon.classList.toggle(`display-none`));
+})
+
+
+document.addEventListener(`click`, (e) => {
+    if(e.target.closest(`.btn--menu`)) {
+        btnsMenu.forEach(btn => btn.classList.toggle(`display-none`));
+        mainSection.classList.toggle(`main--show-asid`);
+    }
+})
+
+
+btnThemeSwitcher.addEventListener(`click`, () => {
+    mainSection.classList.toggle(`main--dark`);
+    IconsThemeSwitcher.forEach(icon => icon.classList.toggle(`icon--theme-switcher-light`));
+    circleThemeSwitcher.classList.toggle(`theme-switcher--circle-dark`);
 })
